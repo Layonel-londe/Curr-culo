5a1e47b7518d588b3b6a4826812681a958872537
Layonel Londe <157087322+Layonel-londe@users.noreply.github.com>
2024-06-06 11:23:32 -0300

    atualizando

--- validate.js/.js
+++ validate.js/.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const assunto = document.getElementById('assunto').value;
         const mensagem = document.getElementById('mensagem').value;
 
-        // You can perform validation here if needed
+        
 
         // Create an object to store form data
         const formData = {
@@ -19,13 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
             assunto: assunto,
             mensagem: mensagem
         };
-
-        // You can perform additional processing here, like sending the data to a server
-
-        // For demonstration purposes, log form data to the console
         console.log(formData);
-
-        // Optionally, you can clear the form fields after submission
         form.reset();
     });
 });
